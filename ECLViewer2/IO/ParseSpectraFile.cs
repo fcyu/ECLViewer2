bc0308117fd55ac1f48196e56dda8567afd87c82
Fengchao <fyuab@connect.ust.hk>
2018-10-02 21:04:17 +0800

    Fix a serious bug due to the difference between index and scan num.

--- ECLViewer2/IO/ParseSpectraFile.cs
+++ ECLViewer2/IO/ParseSpectraFile.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using pwiz.CLI.data;
 using pwiz.CLI.msdata;
 
 namespace ECLViewer.IO
@@ -12,6 +14,7 @@ namespace ECLViewer.IO
     {
 
         private Dictionary<int, Dictionary<float, int>> spectraMap = new Dictionary<int, Dictionary<float, int>>();
+        private Regex scanNumPattern = new Regex(@"scan=([0-9]+)");
 
         public ParseSpectraFile(string mzmlPath, HashSet<int> scanNumSet, string ext)
         {
@@ -20,21 +23,26 @@ namespace ECLViewer.IO
             {
                 MSDataFile msd = new MSDataFile(mzmlPath);
                 SpectrumList spectrumList = msd.run.spectrumList;
-                foreach (int scanNum in scanNumSet)
+                for (int i = 0; i < spectrumList.size(); ++i)
                 {
-                    currentScanNum = scanNum;
-                    Spectrum spectrum;
-                    spectrum = spectrumList.spectrum(scanNum - 1, true); // caution!!
-
-                    MZIntensityPairList mzIntensityPairs = new MZIntensityPairList();
-                    spectrum.getMZIntensityPairs(ref mzIntensityPairs);
-                    Dictionary<float, int> originalPeakList = new Dictionary<float, int>(mzIntensityPairs.Count);
-                    foreach (MZIntensityPair mzIntensity in mzIntensityPairs)
+                    Spectrum spectrum = spectrumList.spectrum(i, true);
+                    if (int.Parse(spectrum.cvParams[0].value) == 2)
                     {
-                        originalPeakList[(float) mzIntensity.mz] = (int) mzIntensity.intensity;
-                    }
+                        Match match = scanNumPattern.Match(spectrum.id);
+                        currentScanNum = int.Parse(match.Groups[1].Value);
+                        if (scanNumSet.Contains(currentScanNum))
+                        {
+                            MZIntensityPairList mzIntensityPairs = new MZIntensityPairList();
+                            spectrum.getMZIntensityPairs(ref mzIntensityPairs);
+                            Dictionary<float, int> originalPeakList = new Dictionary<float, int>(mzIntensityPairs.Count);
+                            foreach (MZIntensityPair mzIntensity in mzIntensityPairs)
+                            {
+                                originalPeakList[(float)mzIntensity.mz] = (int)mzIntensity.intensity;
+                            }
 
-                    spectraMap[scanNum] = originalPeakList;
+                            spectraMap[currentScanNum] = originalPeakList;
+                        }
+                    }
                 }
             }
             catch (Exception ex)
